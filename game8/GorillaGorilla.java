32fa573e600beed3d94e6c1b5b3dca1619fd4a55
yukaf <18138@hakodate.kosen-ac.jp>
2023-01-12 10:27:53 +0900

    ゴリラに当たったら果物消去

--- game8/GorillaGorilla.java
+++ game8/GorillaGorilla.java
@@ -24,5 +24,34 @@ public class GorillaGorilla extends Actor
             setRotation(0);
             move(-5);
         }
+        
+        Actor actor = getOneIntersectingObject(peach.class );
+        Actor actor1 = getOneIntersectingObject(banana.class );
+        Actor actor2 = getOneIntersectingObject(bug.class );
+        Actor actor3 = getOneIntersectingObject(goldapple.class );
+        Actor actor4 = getOneIntersectingObject(kiwi.class );
+        Actor actor5 = getOneIntersectingObject(strawberry.class );
+        //Actor actor6 = getOneIntersectingObject(apple.class );
+        if( actor != null ){
+            getWorld().removeObject( actor );
+        }  
+        if( actor1 != null ){
+            getWorld().removeObject( actor1 );
+        }  
+        if( actor2 != null ){
+            getWorld().removeObject( actor2 );
+        }  
+        if( actor3 != null ){
+            getWorld().removeObject( actor3 );
+        }  
+        if( actor4 != null ){
+            getWorld().removeObject( actor4 );
+        }  
+        if( actor5 != null ){
+            getWorld().removeObject( actor5 );
+        }  
+        //if( actor6 != null ){
+        //    getWorld().removeObject( actor6 );
+        //}  
     }
 }
