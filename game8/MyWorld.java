7bf89dd4b6f030c58b5ece6fdbaca4aac5a1d6e7
yukaf <18138@hakodate.kosen-ac.jp>
2023-02-16 09:14:00 +0900

    更新

--- game8/MyWorld.java
+++ game8/MyWorld.java
@@ -1,28 +1,30 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class MyWorld extends World
-{
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    
-    public Actor titlelogo = null;
-    
-    public MyWorld()
-    {
-        super(1200, 700, 1);
-        titlelogo = new title();
-        addObject( titlelogo, 600, 200 );
-        addObject( new GorillaGorilla(), 150, 570 );
-        addObject( new apple(), 150, 200 );
+            import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+            import java.text.ParseException;
+            import java.util.Timer;
+            import java.util.TimerTask;
+            /**
+             * Write a description of class MyWorld here.
+             * 
+             * @author (your name) 
+             * @version (a version number or a date)
+             */
+            public class MyWorld extends World
+        {
+        
+            /**
+             * Constructor for objects of class MyWorld.
+             * 
+             */
+            
+            public Actor titlelogo = null;
+            
+            public MyWorld()
+            {
+                super(1200, 700, 1);
+                titlelogo = new title();
+                addObject( titlelogo, 600, 200 );
+                addObject( new GorillaGorilla(), 150, 570 );
+                //addObject( new apple(), 150, 200 );
     }
     
     public void act()
--- game8/peach.java
+++ game8/peach.java
@@ -14,19 +14,20 @@ public class peach extends Actor
      */
     public void act() 
     {
-         // Add your action code here.
+                // Add your action code here.
         //getWorld().addObject( new apple(), 150, 300);
         //上のプログラムにx（ランダムで決まる数＋定数）秒後にこれの分を実行するという条件を付ければ、
         //ランダムでりんごを発生させることができそう
         //座標appleX（ランダム）にリンゴを生成
-        
-        int peachX = 200;
+       
+        int peachX = 150;
         int x = getX();
         int y = getY();
         setLocation(x, y + 3);
         if ( isAtEdge() ){
             getWorld().removeObject ( this );
         }
+
   
     }    
 }
